bfdf4054a4a8faae649e7e72a96a5a08eff2c7fb
kaeross <kaeross@icloud.com>
2017-06-17 22:08:31 +0100

    Interactive Form 0.3
    Payment method shown and hidden according to selected method

--- js/app.js
+++ js/app.js
@@ -3,22 +3,18 @@ const form = $('form');
 const textInput = $('[type=text]');
 const firstText = form.children().children().first(textInput);
 const jobRoleSelect = $('#title');
+const paymentField = $('#payment');
+const otherTextField = $('#other-title');
 
-//create text box for 'other' option in select
+//show or hide other-title text field
 function otherText() {
-	if (jobRoleSelect.val() === "other") {
-		const firstFieldset = form.children(':first');
-		//create text field
-		const otherTextField = document.createElement('input');
-		otherTextField.type = 'text';
-		otherTextField.setAttribute('id', 'other-title');
-		otherTextField.setAttribute('placeholder', 'Your Job Role');
-		//append text field below select
-		firstFieldset.append(otherTextField);
+	if (jobRoleSelect.val() != "other") {
+		//had originally created text field using javascript but tutor told us to hardcode it to the HTML
+		otherTextField.hide();
+	//show other-title if selected
+	} else if (jobRoleSelect.val() === "other")  {
+		otherTextField.show();
 		otherTextField.focus();
-		//remove other text box if not selected
-	} else if (jobRoleSelect.val() != "other")  {
-		$('#other-title').remove();
 	}
 }
 
@@ -27,31 +23,78 @@ function showHideColor() {
 	const colorOption = $('#color > option');
 	const jsPuns = colorOption.slice(0,3);
 	const heartJS = colorOption.slice(3,6);
-	function showHide(showArray, hideArray) {
+	function showHideColorOption (showItem, hideItem) {
 		colorOption.hide();
-		showArray.show();
-		showArray[0].setAttribute('selected', 'selected');
-		hideArray.hide();
+		showItem.show();
+		showItem[0].setAttribute('selected', 'selected');
+		hideItem.hide();
 	}
 	if ($('#design').val() === 'Select Theme') {
 		$('#colors-js-puns').hide();
-	} else {
+	} 
+	else {
 		$('#colors-js-puns').show();
 	}
 	if ($('#design').val() === 'js puns')  {
-		showHide(jsPuns, heartJS);
+		showHideColorOption(jsPuns, heartJS);
 	} else if ($('#design').val() === 'heart js')  {
-		showHide(heartJS, jsPuns);
+		showHideColorOption(heartJS, jsPuns);
 	}
 }
 
 //activities
 function findDayTime() {
 	const activities = $('.activities > label');
-	const dayAndTime; 
-	console.log(day + ' ' + time);
+	const matchingAM = /Tuesday 9am-12pm/g;
+	const matchingPM = /Tuesday 1pm-4pm/g;
+	const amArray = [];
+	const pmArray = [];
+	//loop over each activities text
 
+	for(let i = 0; i <= activities.length; i +=	1){
+		const testAM = matchingAM.test(activities[i].textContent);
+		const testPM = matchingPM.test(activities[i].textContent);
+		if (testAM === true) {
+			console.log(activities[i]);
+		}
+		if (testPM === true) {
+			console.log(activities[i]);
+		}	
+	}
 	//disable checkbox checkbox.prop('disabled', true)
+}
+
+/*    PAYMENTS 
+	If payment method isn't selected, hide payments   */
+
+
+function showHidePayment() {
+	const paymentVal = $('#payment').val();
+	const paymentMethods = paymentField.siblings('div');
+	const creditCardMethod = paymentMethods[0];
+	const paypalMethod = paymentMethods[1];
+	const bitcoinMethod = paymentMethods[2];
+	function showHidePaymentDiv (showItem) {
+		paymentMethods.hide();
+		showItem.style.display = 'block';
+	}
+	if (paymentVal === 'select_method') {
+		paymentMethods.hide();
+	} else if (paymentVal === 'credit card') {
+		showHidePaymentDiv(creditCardMethod);
+		$('#cc-num').focus();
+	} else if (paymentVal === 'paypal') {
+		showHidePaymentDiv(paypalMethod);
+	} else if (paymentVal === 'bitcoin') {
+		showHidePaymentDiv(bitcoinMethod);
+	}
+
+}
+
+function onChange(object, functionName) {
+	object.on('change', function (e) {
+		functionName();
+	});
 }
 
 //on document ready focus on first text input. If other is already selected load other text box
@@ -59,20 +102,14 @@ $(document).ready(function() {
 	firstText.focus();
 	otherText();
 	showHideColor();
+	showHidePayment();
 });
 
-//A text field that will be revealed when the "Other" option is selected from the "Job Role" drop down menu.
-jobRoleSelect.on('change', function (e) {
-	otherText();
-});
-
-
-$('#design').on('change', function(){
-	showHideColor();
-});
+onChange(jobRoleSelect, otherText);
+onChange($('#design'), showHideColor);
+onChange($('#payment'), showHidePayment);
 
 
 //form saves to browser for refresh
 
-    
-   
+
