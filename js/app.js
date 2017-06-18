f1b1304d45f06a1f06e9586149aab9a291482b47
kaeross <kaeross@icloud.com>
2017-06-18 23:31:30 +0100

    Activities running totals function

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 const form = $('form'); 
-//focus on first text field
 const textInput = $('[type=text]');
 const firstText = form.children().children().first(textInput);
 const jobRoleSelect = $('#title');
 const paymentField = $('#payment');
-const otherTextField = $('#other-title');	
-const allCheckBoxes = $('input[type=checkbox]');
+const otherTextField = $('#other-title');
+const allCheckBoxes = $('input[type=checkbox]');	
 const jsFrameworks = $('input[name=js-frameworks]');
 const jsLibs = $('input[name=js-libs]');
 const express = $('input[name=express]');
+const node = $('input[name=node]');
 const mainConference = $('input[name=all]');
 const buildTools = $('input[name=build-tools]');
 const npm = $('input[name=npm]');
@@ -87,14 +87,31 @@ Activities - display running total
 **********************************/
 
 function activitiesTotal() {
-	const getTotal = () => {
-		let runningTotal;
+	$('.total').remove();
+	function getTotal() {
+		let runningTotal = 0;
 		//if mainConference is selected running total +$200
-		if()
-	};
+		if(mainConference.is(':checked')) {
+			runningTotal += 200;
+		} 
+		function plus100(checkbox){
+			if(checkbox.is(':checked')) {
+				runningTotal += 100;
+			} 
+		}
+		plus100(node);
+		plus100(npm);
+		plus100(jsFrameworks);
+		plus100(jsLibs);
+		plus100(express);
+		plus100(buildTools);
+
+		return runningTotal;
+	}
+	const getTotalvalue = getTotal();
 	//create total section and append to activies fieldset
-	let totalDiv = '<h3>Total: ';
-	totalDiv += '$100';
+	let totalDiv = '<h3 class="total">Total: $';
+	totalDiv += getTotalvalue;
 	totalDiv += '</h3>';
 	$('.activities').append(totalDiv);
 }
@@ -125,20 +142,42 @@ function showHidePayment() {
 	}
 }
 
-/****************************************
-FUNCTIONS TO BE CALLED ON DOCUMENT READY
-****************************************/
+/****************
+ FORM VALIDATION 
+*****************/
+
+//NAME FIELD
+
+//EMAIL FIELD
+const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+//AT LEAST ONE CHECKBOX SELECTED
+
+//IF CREDITCARD
+	//ALL FIELDS FILLED OUT
+	//CARD NUMBER BETWEEN 13 AND 16
+	//ZIPCODE FIELD 5 DIGIT NUMBER
+	//CVV EXACTLY 3 DIGITS
+
+
+
+
+/******************************************
+ FUNCTIONS TO BE CALLED WHEN DOCUMENT READY
+******************************************/
 $(document).ready(function() {
 	firstText.focus();
 	otherText();
 	showHideColor();
 	showHidePayment();
 	disableConflictingActivities();
+	activitiesTotal();
 });
 
 /********************************
-FUNCTIONS TO BE CALLED ON CHANGE
+ FUNCTIONS TO BE CALLED ON CHANGE
 ********************************/
+
 //function to call functions on change
 function onChange(object, functionName) {
 	object.on('change', function (e) {
@@ -152,10 +191,8 @@ onChange($('#payment'), showHidePayment);
 onChange((allCheckBoxes), disableConflictingActivities);
 onChange((allCheckBoxes), activitiesTotal);
 
-//form saves to browser for refresh
 
 
-const emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
 
